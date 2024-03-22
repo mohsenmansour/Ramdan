0a6f0668d6d3cb8e26a9d291c682bfbccebb7af3
mohsenmansour <abdalmohsenmansour1@gmail.com>
2024-03-23 00:30:04 +0200

    fast

---
+++ sign1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+ main() 
+ { 
+     int K = 64; 
+     printf("|%+20d|\n", K);  // right justification with +ve sign 
+     printf("|%-20d|\n", K);  // left justification with no sign 
+     printf("|%-+20d|\n", K); // left justification with +ve sign 
+     printf("|%+-20d|\n", K); // left justification with +ve sign 
+     printf("/%20d|\n", -K); // right justification with -ve sign 
+     printf("|%-20d|\n", -K); // left justification with -ve sign 
+ } 
